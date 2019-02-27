dab82578a4f95b8311f414174143b71d6f6b70c2
wangenyong <wangenyong@gmail.com>
2019-02-27 15:59:15 +0800

    Create repository.

---
+++ src/main/java/com/dswey/springbootjwt/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.dswey.springbootjwt.repository;
+
+import com.dswey.springbootjwt.model.Role;
+import com.dswey.springbootjwt.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
